49c6b6a226142df55e4fb8130794fa6df5cf750f
k-mill1 <kaisamill@gmail.com>
2021-08-05 16:16:23 +0100

    Make code more reusable

--- quizCode.js
+++ quizCode.js
@@ -2,149 +2,102 @@
  * Displays only the intro page when you open the page
  */
 window.onload = function() {
-  document.getElementById('intro').style.display = 'block';
-  document.getElementById('question1').style.display = 'none';
-  document.getElementById('question2').style.display = 'none';
-  document.getElementById('question3').style.display = 'none';
-  document.getElementById('question4').style.display = 'none';
-  document.getElementById('question5').style.display = 'none';
-  document.getElementById('result1').style.display = 'none';
-  document.getElementById('result2').style.display = 'none';
-  document.getElementById('result3').style.display = 'none';
-  document.getElementById('result4').style.display = 'none';
-  document.getElementById('results-intro').style.display = 'none';
+  showElement('intro');
+  hideElement('question1');
+  hideElement('question2');
+  hideElement('question3');
+  hideElement('question4');
+  hideElement('question5');
+  hideElement('results-intro');
+  hideElement('result1');
+  hideElement('result2');
+  hideElement('result3');
+  hideElement('result4');
 }
+
 /**
- * hides intro and show question 1
+ * hides element by id
  */
- function startQuiz() {
-    document.getElementById("intro").style.display = "none";
-    document.getElementById("question1").style.display = "block";
-  }
+function hideElement(id) {
+  document.getElementById(id).style.display = 'none';
+}
 
-  /**
- * hides question 1 shows question2
+/**
+ * shows element by id
  */
-function question2() {
-    document.getElementById("question1").style.display = "none";
-    document.getElementById("question2").style.display = "block";
-  }
+function showElement(id) {
+  document.getElementById(id).style.display = 'block';
+}
 
-    /**
- * hides question 2 shows question3
+/**
+ * hides current question and shows the next
  */
-function question3() {
-    document.getElementById("question2").style.display = "none";
-    document.getElementById("question3").style.display = "block";
-  }
+function nextPage(hide,show) {
+  hideElement(hide);
+  showElement(show);
+}
 
-    /**
- * hides question 3 shows question4
- */
-function question4() {
-    document.getElementById("question3").style.display = "none";
-    document.getElementById("question4").style.display = "block";
-  }
-
-      /**
- * hides question 4 shows question5
- */
-function question5() {
-    document.getElementById("question4").style.display = "none";
-    document.getElementById("question5").style.display = "block";
-  }
-  
-  /**
- * hides question 2 shows results
- */
-  function results() {
-    calculateResults();
-    document.getElementById("question5").style.display = "none";
-    document.getElementById("results-intro").style.display = "block";
-    
-  }
-
-  /**
+/**
  * calculates and displays the results
  */
-function calculateResults() {
-  let array=[]
-  const radios1 = document.querySelectorAll('input[name="quest1"]');
-  let q1Value;
-  for (const rb of radios1) {
-      if (rb.checked) {
-          q1Value = rb.value;
-          array.push(q1Value);
-          break;
-      }
+ function results(lastQuestion,questTotalNum) {
+  hideElement(lastQuestion);
+  showElement('results-intro');
+
+  let array = [];
+  //adds the answer to each question into the array
+  for(x = 1; x <= questTotalNum; x++){
+    getRadioVal(String(x),array);
   }
   
-  const radios2 = document.querySelectorAll('input[name="quest2"]');
-  let q2Value;
-  for (const rb of radios2) {
-      if (rb.checked) {
-          q2Value = rb.value;
-          array.push(q2Value);
-          break;
-      }
-  }
+  //calculates how many times each answer appears in the array
+  answer1Count = howMany(array,1);
+  answer2Count = howMany(array,2);
+  answer3Count = howMany(array,3);
 
-  const radios3 = document.querySelectorAll('input[name="quest3"]');
-  let q3Value;
-  for (const rb of radios3) {
-      if (rb.checked) {
-          q3Value = rb.value;
-          array.push(q3Value);
-          break;
-      }
-  }
-
-  const radios4 = document.querySelectorAll('input[name="quest4"]');
-  let q4Value;
-  for (const rb of radios4) {
-      if (rb.checked) {
-          q4Value = rb.value;
-          array.push(q4Value);
-          break;
-      }
-  }
-
-  const radios5 = document.querySelectorAll('input[name="quest5"]');
-  let q5Value;
-  for (const rb of radios5) {
-      if (rb.checked) {
-          q5Value = rb.value;
-          array.push(q5Value);
-          break;
-      }
-  }
-  var a = 0;
-  for(var i = 0; i < array.length; ++i){
-      if(array[i] == 1)
-          a++;
-  }
-  var b = 0;
-for(var i = 0; i < array.length; ++i){
-    if(array[i] == 2)
-        b++;
+  //displays the correct result
+  getResult(answer1Count,answer2Count,answer3Count);
 }
-  var c = 0;
-  for(var i = 0; i < array.length; ++i){
-      if(array[i] == 3)
-          c++;
-  }
 
-  let result = Number(q1Value) + Number(q2Value) + Number(q3Value) + Number(q4Value) + Number(q5Value);
-
-  if (a > 2 ) {
-    document.getElementById("result1").style.display = "block";
-  } else if (b > 2) {
-    document.getElementById("result2").style.display = "block";
-  } else if (c > 2) {
-    document.getElementById("result3").style.display = "block";
-  } else {
-    document.getElementById("result4").style.display = "block";
-  }
-
+/**
+ * adds the selected answer for a specified question number into an array
+ */
+ function getRadioVal(questionNum,array) {
+  let name = "quest" + questionNum
+  let radios = document.getElementsByName(name);
+  
+  for (let f=0; f<radios.length; f++) {
+      if (radios[f].checked) { 
+          array.push(radios[f].value); 
+          break; 
+      }
+    }
 }
+
+/**
+ * counts how many answers of the same value there are in an array
+ */
+  function howMany(array,value) {
+    let counter=0;
+    for(let i =0; i < array.length; ++i){
+      if(array[i] == value)
+          counter++;
+    }
+    return counter;
+  }
+
+/**
+ * displays the correct result 
+ */
+  function getResult(answer1,answer2,answer3) {
+    if (answer1 > 2 ) {
+      showElement('result1');
+    } else if (answer2 > 2) {
+      showElement('result2');
+    } else if (answer3 > 2) {
+      showElement('result3');
+    } else {
+      showElement('result4');
+  }
+  }
 
